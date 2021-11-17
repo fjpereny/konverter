4a01e7d41b727a61915fbebc3259d6f29ab1113a
Frank Pereny <fjpereny@gmail.com>
2021-11-17 22:00:56 +0800

    Added

--- konverter_en_US.ts
+++ konverter_en_US.ts
@@ -5,26 +5,31 @@
     <name>AboutDialog</name>
     <message>
         <location filename="aboutdialog.ui" line="14"/>
+        <location filename="builds/build-konverter-Desktop_Qt_6_2_1_GCC_64bit-Debug/konverter_autogen/include/ui_aboutdialog.h" line="111"/>
         <source>About konverter</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="aboutdialog.ui" line="48"/>
+        <location filename="builds/build-konverter-Desktop_Qt_6_2_1_GCC_64bit-Debug/konverter_autogen/include/ui_aboutdialog.h" line="112"/>
         <source>Konverter 0.1</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="aboutdialog.ui" line="58"/>
+        <location filename="builds/build-konverter-Desktop_Qt_6_2_1_GCC_64bit-Debug/konverter_autogen/include/ui_aboutdialog.h" line="113"/>
         <source>Copyright © 2021 &lt;a href=&quot;mailto:fjpereny@gmail.com&quot;&gt;Frank Pereny&lt;/a&gt;</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="aboutdialog.ui" line="68"/>
+        <location filename="builds/build-konverter-Desktop_Qt_6_2_1_GCC_64bit-Debug/konverter_autogen/include/ui_aboutdialog.h" line="114"/>
         <source>&lt;a href=&quot;https://github.com/fjpereny/konverter&quot;&gt;https://github.com/fjpereny/konverter&lt;/a&gt;</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="aboutdialog.ui" line="81"/>
+        <location filename="builds/build-konverter-Desktop_Qt_6_2_1_GCC_64bit-Debug/konverter_autogen/include/ui_aboutdialog.h" line="115"/>
         <source>This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 
 This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.</source>
@@ -35,128 +40,155 @@ This program is distributed in the hope that it will be useful, but WITHOUT ANY 
     <name>DataTableWindow</name>
     <message>
         <location filename="datatablewindow.ui" line="14"/>
+        <location filename="builds/build-konverter-Desktop_Qt_6_2_1_GCC_64bit-Debug/konverter_autogen/include/ui_datatablewindow.h" line="287"/>
         <source>Konverter</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="datatablewindow.ui" line="23"/>
+        <location filename="builds/build-konverter-Desktop_Qt_6_2_1_GCC_64bit-Debug/konverter_autogen/include/ui_datatablewindow.h" line="299"/>
         <source>Unit Type or Category</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="datatablewindow.ui" line="48"/>
         <location filename="datatablewindow.ui" line="225"/>
+        <location filename="builds/build-konverter-Desktop_Qt_6_2_1_GCC_64bit-Debug/konverter_autogen/include/ui_datatablewindow.h" line="300"/>
+        <location filename="builds/build-konverter-Desktop_Qt_6_2_1_GCC_64bit-Debug/konverter_autogen/include/ui_datatablewindow.h" line="319"/>
         <source>Add</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="datatablewindow.ui" line="55"/>
         <location filename="datatablewindow.ui" line="235"/>
+        <location filename="builds/build-konverter-Desktop_Qt_6_2_1_GCC_64bit-Debug/konverter_autogen/include/ui_datatablewindow.h" line="301"/>
+        <location filename="builds/build-konverter-Desktop_Qt_6_2_1_GCC_64bit-Debug/konverter_autogen/include/ui_datatablewindow.h" line="320"/>
         <source>Del</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="datatablewindow.ui" line="96"/>
+        <location filename="builds/build-konverter-Desktop_Qt_6_2_1_GCC_64bit-Debug/konverter_autogen/include/ui_datatablewindow.h" line="303"/>
         <source>Decimals</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="datatablewindow.ui" line="123"/>
+        <location filename="builds/build-konverter-Desktop_Qt_6_2_1_GCC_64bit-Debug/konverter_autogen/include/ui_datatablewindow.h" line="304"/>
         <source>Master Unit</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="datatablewindow.ui" line="130"/>
+        <location filename="builds/build-konverter-Desktop_Qt_6_2_1_GCC_64bit-Debug/konverter_autogen/include/ui_datatablewindow.h" line="306"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;All units in the database are linked to the master unit. To prevent circular logic errors, the master unit may not be directly modified.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="datatablewindow.ui" line="133"/>
+        <location filename="builds/build-konverter-Desktop_Qt_6_2_1_GCC_64bit-Debug/konverter_autogen/include/ui_datatablewindow.h" line="309"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;All units in the database are linked to the master unit.  To prevent circular logic errors, the master unit may not be directly modified.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="datatablewindow.ui" line="182"/>
+        <location filename="builds/build-konverter-Desktop_Qt_6_2_1_GCC_64bit-Debug/konverter_autogen/include/ui_datatablewindow.h" line="313"/>
         <source>Unit Name</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="datatablewindow.ui" line="187"/>
+        <location filename="builds/build-konverter-Desktop_Qt_6_2_1_GCC_64bit-Debug/konverter_autogen/include/ui_datatablewindow.h" line="315"/>
         <source>Conversion Value</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="datatablewindow.ui" line="192"/>
+        <location filename="builds/build-konverter-Desktop_Qt_6_2_1_GCC_64bit-Debug/konverter_autogen/include/ui_datatablewindow.h" line="317"/>
         <source>Notes</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="datatablewindow.ui" line="215"/>
+        <location filename="builds/build-konverter-Desktop_Qt_6_2_1_GCC_64bit-Debug/konverter_autogen/include/ui_datatablewindow.h" line="318"/>
         <source>Enable Editing</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="datatablewindow.ui" line="245"/>
+        <location filename="builds/build-konverter-Desktop_Qt_6_2_1_GCC_64bit-Debug/konverter_autogen/include/ui_datatablewindow.h" line="321"/>
         <source>Save</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="datatablewindow.ui" line="266"/>
+        <location filename="builds/build-konverter-Desktop_Qt_6_2_1_GCC_64bit-Debug/konverter_autogen/include/ui_datatablewindow.h" line="322"/>
         <source>File</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="datatablewindow.ui" line="274"/>
+        <location filename="builds/build-konverter-Desktop_Qt_6_2_1_GCC_64bit-Debug/konverter_autogen/include/ui_datatablewindow.h" line="323"/>
         <source>Edit</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="datatablewindow.ui" line="287"/>
+        <location filename="builds/build-konverter-Desktop_Qt_6_2_1_GCC_64bit-Debug/konverter_autogen/include/ui_datatablewindow.h" line="324"/>
         <source>Help</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="datatablewindow.ui" line="310"/>
+        <location filename="builds/build-konverter-Desktop_Qt_6_2_1_GCC_64bit-Debug/konverter_autogen/include/ui_datatablewindow.h" line="288"/>
         <source>Close</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="datatablewindow.ui" line="315"/>
+        <location filename="builds/build-konverter-Desktop_Qt_6_2_1_GCC_64bit-Debug/konverter_autogen/include/ui_datatablewindow.h" line="289"/>
         <source>Change Master Unit</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="datatablewindow.ui" line="320"/>
+        <location filename="builds/build-konverter-Desktop_Qt_6_2_1_GCC_64bit-Debug/konverter_autogen/include/ui_datatablewindow.h" line="290"/>
         <source>Undo</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="datatablewindow.ui" line="325"/>
+        <location filename="builds/build-konverter-Desktop_Qt_6_2_1_GCC_64bit-Debug/konverter_autogen/include/ui_datatablewindow.h" line="291"/>
         <source>Redo</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="datatablewindow.ui" line="330"/>
+        <location filename="builds/build-konverter-Desktop_Qt_6_2_1_GCC_64bit-Debug/konverter_autogen/include/ui_datatablewindow.h" line="292"/>
         <source>Cut</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="datatablewindow.ui" line="335"/>
+        <location filename="builds/build-konverter-Desktop_Qt_6_2_1_GCC_64bit-Debug/konverter_autogen/include/ui_datatablewindow.h" line="293"/>
         <source>Copy</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="datatablewindow.ui" line="338"/>
+        <location filename="builds/build-konverter-Desktop_Qt_6_2_1_GCC_64bit-Debug/konverter_autogen/include/ui_datatablewindow.h" line="295"/>
         <source>Ctrl+C</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="datatablewindow.ui" line="343"/>
+        <location filename="builds/build-konverter-Desktop_Qt_6_2_1_GCC_64bit-Debug/konverter_autogen/include/ui_datatablewindow.h" line="297"/>
         <source>Paste</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="datatablewindow.ui" line="348"/>
+        <location filename="builds/build-konverter-Desktop_Qt_6_2_1_GCC_64bit-Debug/konverter_autogen/include/ui_datatablewindow.h" line="298"/>
         <source>About</source>
         <translation type="unfinished"></translation>
     </message>
@@ -165,26 +197,31 @@ This program is distributed in the hope that it will be useful, but WITHOUT ANY 
     <name>NewUnitDialog</name>
     <message>
         <location filename="newunitdialog.ui" line="17"/>
+        <location filename="builds/build-konverter-Desktop_Qt_6_2_1_GCC_64bit-Debug/konverter_autogen/include/ui_newunitdialog.h" line="132"/>
         <source>Create New Unit</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="newunitdialog.ui" line="30"/>
+        <location filename="builds/build-konverter-Desktop_Qt_6_2_1_GCC_64bit-Debug/konverter_autogen/include/ui_newunitdialog.h" line="133"/>
         <source>Unit Name</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="newunitdialog.ui" line="44"/>
+        <location filename="builds/build-konverter-Desktop_Qt_6_2_1_GCC_64bit-Debug/konverter_autogen/include/ui_newunitdialog.h" line="134"/>
         <source>Master Unit</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="newunitdialog.ui" line="56"/>
+        <location filename="builds/build-konverter-Desktop_Qt_6_2_1_GCC_64bit-Debug/konverter_autogen/include/ui_newunitdialog.h" line="135"/>
         <source>Clone Data From Existing</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="newunitdialog.ui" line="67"/>
+        <location filename="builds/build-konverter-Desktop_Qt_6_2_1_GCC_64bit-Debug/konverter_autogen/include/ui_newunitdialog.h" line="136"/>
         <source>Existing Unit Data</source>
         <translation type="unfinished"></translation>
     </message>
